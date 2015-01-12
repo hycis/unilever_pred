58e55f9d79b730f4a428d9046b1a8c86e26d3ce9
Lucas Tan <lucastan@users.noreply.github.com>
2015-01-13 00:03:43 +0800

    updated utils

--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 from __future__ import unicode_literals
 
+import csv as csv_mod
+import codecs
 import os
+
+
+def is_float(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 
 def write_pred(filename, ids, preds):
@@ -9,3 +19,23 @@ def write_pred(filename, ids, preds):
         f.write("ID,Overall.Opinion\n")
         for (id, p) in zip(ids, preds):
             f.write("{},{}\n".format(int(id), int(p)))
+
+
+def _utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf-8')
+
+
+def csv_reader_utf8(file_path, **kwargs):
+    """
+    Adapted from http://docs.python.org/2/library/csv.html
+    """
+
+    f = codecs.open(file_path, encoding='utf8', mode='rb', errors='ignore')
+
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv_mod.reader(_utf_8_encoder(f), **kwargs)
+
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
