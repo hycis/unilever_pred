60608aed1bb691087e46a1b6112f5c96ca0d8cf2
Lucas Tan <lucastan@users.noreply.github.com>
2015-01-18 17:12:34 +0800

    updated feature_select

--- feature_select.py
+++ feature_select.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
+import sys
+
 import numpy as np
 from sklearn import svm
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 
 from data import load_names, DataSet, TRAIN_FILENAME, TEST_FILENAME
+from utils import filter_index
+
+
+def avgfeats(filename, names, features, labels):
+    with open(filename, "wb") as f:
+        f.write("label")
+        for name in names:
+            f.write(","+name)
+        f.write("\n")
+
+        for s in xrange(1, 8):
+            idxes = filter_index(labels, lambda x : x == s)
+            feats = features[idxes, :]
+            avg = np.average(feats, axis=0)
+            f.write("{}".format(s))
+            for a in avg:
+                f.write(",{:.4f}".format(a))
+            f.write("\n")
 
 
 def main():
@@ -12,6 +35,23 @@ def main():
     test = DataSet("test_neutral.npy")
     feat_indexes = train.features_no_ingre_prob_indexes
     names_no_ingre_prob = names[feat_indexes, :]
+
+    avgfeats("avgfeats.csv", names_no_ingre_prob, train.features_no_ingre_prob, train.labels)
+
+    sel = SelectKBest(chi2, k=2)
+    sel.fit(train.features_no_ingre_prob, train.labels)
+
+    with open("feats-scores.csv", "wb") as f:
+        for name in names_no_ingre_prob:
+            f.write(name + ",")
+        f.write("\n")
+        for s in sel.scores_:
+            f.write("{:.4f},".format(s))
+        f.write("\n")
+
+    bestidxes = list(np.argsort(sel.scores_))
+    bestidxes.reverse()
+    avgfeats("bestfeats.csv", names_no_ingre_prob[bestidxes, :], train.features_no_ingre_prob[:, bestidxes], train.labels)
 
     clf = svm.LinearSVC(C=0.01, penalty="l1", dual=False)
     clf.fit(train.features_no_ingre_prob, train.labels)
