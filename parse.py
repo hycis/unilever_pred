84e655ae23a578d56b4a90a3d9889a44820d3c36
Lucas Tan <lucastan@users.noreply.github.com>
2015-01-19 22:01:59 +0800

    updated parse

--- parse.py
+++ parse.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-# TODO: fill in NA with sampled data from each class
-# TODO: sum up the problems
+# TODO: interpolate the agree columns only
+# TODO: average the columns, group by category
+
 
 from __future__ import unicode_literals
 
 import csv as csv_mod
+import itertools
 import re
 
 import numpy as np
 
 from utils import csv_reader_utf8, is_float
+import data
+from data import DataSet
 
 AGREE_RE = re.compile(r'^.*?\((?P<number>\d+)\)$')
 FIRST_NUMBER_RE = re.compile(r'(?P<number>\d+)')
@@ -220,52 +224,56 @@ def parse_marriage(s):
     raise Exception("invalid marriage: " + cell)
 
 
+SCHEMA = (
+    # response ID
+    ([0], int),
+    # product ID
+    ([1], lambda s: int(s[2:])),
+    # parse ingredients
+    (xrange(2, 155), parse_number_or_zero),
+    ([155, 229], parse_bool),
+    ([156], parse_how_much),
+    ([157], parse_times_used),
+    (xrange(158, 229), parse_agree),
+    (xrange(230, 254), parse_problem),
+    (xrange(254, 259), parse_na_bool_or_number),
+    ([259], parse_soak),
+    (xrange(260, 264), parse_bool),
+
+    # Rinse method, dry method
+    ([264], parse_rinse),
+    ([265], parse_dry),
+
+    # income
+    ([266], parse_income),
+
+    # education, marrital status, employment, ... set to 0
+    # (xrange(267, 272), None),
+
+    # Employment hours
+    ([268], parse_na_or_number),
+
+    # occupants
+    (xrange(272, 277), parse_na_or_number),
+
+    (xrange(277, 288), parse_agree),
+    ([288], parse_dissolve),
+    (xrange(289, 294), parse_agree),
+    ([294], lambda s: {'none': 0, 'no': 0, 'na': -1}.get(s.lower(), 1)),
+    (xrange(295, 297), parse_agree),
+    ([297], parse_bool),
+    ([298], parse_soak),
+    (xrange(299, 301), parse_agree),
+
+    # overall opinion
+    ([301], parse_number_or_zero),
+)
+
+
+AGREE_INDEXES = itertools.chain(map(lambda x: x[0] if x[1] == parse_agree else [], SCHEMA))
+
+
 def read_csv(file_path):
-    formats = (
-        # response ID
-        ([0], int),
-        # product ID
-        ([1], lambda s: int(s[2:])),
-        # parse ingredients
-        (xrange(2, 155), parse_number_or_zero),
-        ([155, 229], parse_bool),
-        ([156], parse_how_much),
-        ([157], parse_times_used),
-        (xrange(158, 229), parse_agree),
-        (xrange(230, 254), parse_problem),
-        (xrange(254, 259), parse_na_bool_or_number),
-        ([259], parse_soak),
-        (xrange(260, 264), parse_bool),
-
-        # Rinse method, dry method
-        ([264], parse_rinse),
-        ([265], parse_dry),
-
-        # income
-        ([266], parse_income),
-
-        # education, marrital status, employment, ... set to 0
-        # (xrange(267, 272), None),
-
-        # Employment hours
-        ([268], parse_na_or_number),
-
-        # occupants
-        (xrange(272, 277), parse_na_or_number),
-
-        (xrange(277, 288), parse_agree),
-        ([288], parse_dissolve),
-        (xrange(289, 294), parse_agree),
-        ([294], lambda s: {'none': 0, 'no': 0, 'na': -1}.get(s.lower(), 1)),
-        (xrange(295, 297), parse_agree),
-        ([297], parse_bool),
-        ([298], parse_soak),
-        (xrange(299, 301), parse_agree),
-
-        # overall opinion
-        ([301], parse_number_or_zero),
-    )
-
     csv = csv_reader_utf8(file_path, dialect=csv_mod.excel)
     # skip first row which is header
     for row in csv:
@@ -278,12 +286,12 @@ def read_csv(file_path):
         for row in csv:
             # allocate array
             parsed_row = [0] * len(row)
-            for format in formats:
+            for format in SCHEMA:
                 for i in format[0]:
                     parsed_row[i] = format[1](row[i])
             parsed_rows.append(parsed_row)
             rownum += 1
-    except Exception as e:
+    except Exception:
         print("row#={} i={} cell={}".format(rownum, i, row[i]))
         raise
 
@@ -293,80 +301,22 @@ def read_csv(file_path):
     return parsed_rows
 
 
-def interpolate_na(orig_data):
-    data = np.copy(orig_data)
-    import random
-    count = len(data[0])
-    random.seed(123)
-
-    for lbl in xrange(1, 8):
-        non_na = []
-
-        for i in xrange(0, count):
-            non_na.append([])
-
-        for row in data:
-            if row[-1] != lbl:
-                continue
-
-            for i in xrange(0, count):
-                if row[i] >= 0:
-                    non_na[i].append(row[i])
-
-        for i in xrange(0, count):
-            if not non_na[i]:
-                non_na[i].append(0)
-
-        for row in data:
-            if row[-1] != lbl:
-                continue
-
-            for i in xrange(0, count):
-                if row[i] < 0:
-                    row[i] = non_na[i][random.randint(0, len(non_na[i]) - 1)]
-    return data
-
-
-def neutral_na(orig_data):
-    data = np.copy(orig_data)
-    count = len(data[0])
-    mins = [1e99] * count
-    maxs = [-1e99] * count
-
-    for row in data:
-        for i in xrange(0, count):
-            if row[i] >= 0:
-                mins[i] = min(mins[i], row[i])
-                maxs[i] = max(maxs[i], row[i])
-
-    for row in data:
-        for i in xrange(0, count):
-            if row[i] < 0:
-                row[i] = (mins[i] + maxs[i]) * .5
-
-    return data
-
-
-def make_na_zero(orig_data):
-    data = np.copy(orig_data)
-    for row in data:
-        for i in xrange(0, len(row)):
-            if row[i] < 0:
-                row[i] = 0
-    return data
-
-
 if __name__ == '__main__':
-    train = np.array(read_csv('train.csv'))
+    train_raw = np.array(read_csv('train.csv'))
     # this row has a score of NA (invalid), change to 5.
-    train[6269, -1] = 5
-    # Last column is the opinion score 0-7 (label)
-    np.save('train.npy', make_na_zero(train))
-    np.save('train_neutral.npy', neutral_na(train))
-    np.save('train_interpolated.npy', interpolate_na(train))
+    train_raw[6269, -1] = 5
+    np.save(data.TRAIN_RAW_FILENAME, train_raw)
 
-    sub = np.array(read_csv('sub.csv'))
-    # Last column is the test label which is all 0
-    np.save('test.npy', make_na_zero(sub))
-    np.save('test_neutral.npy', neutral_na(sub))
-    np.save('test_interpolated.npy', interpolate_na(sub))
+    train = DataSet(data=train_raw)
+    train.transform_na_zero()
+    np.save(data.TRAIN_FILENAME, train.data)
+
+
+    test_raw = np.array(read_csv('sub.csv'))
+    np.save(data.TEST_RAW_FILENAME, test_raw)
+
+    test = DataSet(data=test_raw)
+    test.transform_na_zero()
+    np.save(data.TEST_FILENAME, test.data)
+
+
